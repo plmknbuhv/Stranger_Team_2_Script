85b7b073b3960b2dbb36ea78b2e6a782cfa46848
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2026-04-26 22:34:58 +0900

    Refactor BuildObject properties and comments
    Refactor comments and improve clarity of properties.

--- BuildingSystem/BuildObject.cs
+++ BuildingSystem/BuildObject.cs
@@ -11,9 +11,9 @@ namespace _01_Work.HS.Building
 {
     public class BuildObject : SelectObject, IPlaceable
     {
-        public BuildingDataSO BuildingDataSO { get; private set; } // 이거 나중에 바꿀수도 있음
-        public Queue<WorkingUnit> WorkingUnitList = new Queue<WorkingUnit>(); 
-        protected List<Ground> _groundList = new List<Ground>();
+        public BuildingDataSO BuildingDataSO { get; private set; }
+        public Queue<WorkingUnit> WorkingUnitList = new Queue<WorkingUnit>();  // 현재 건물에 일 하고 있는 유닛 리스트
+        protected List<Ground> _groundList = new List<Ground>();  // 여러 칸을 차지하는 건물 때문에 List로 관리
         
         public Vector2Int ActualSize { get; private set; }
 
@@ -21,8 +21,9 @@ namespace _01_Work.HS.Building
         // 이후에 건물 설치에 시간이 소모되는 기능 추가를 염두하고 만듬
         // 사용 안하는중
 
-        public Action OnCanWorkEvent;
-        
+        public Action OnCanWorkEvent; // 유닛이 배치되어서 꽉 찰 경우 발행되는 이벤트
+
+        // 건물 이전에 미리보기 시 실해되는 함수 
         public virtual void PrepareBuild(BuildingDataSO buildingSO)
         {
             BuildingDataSO = buildingSO;
@@ -31,6 +32,7 @@ namespace _01_Work.HS.Building
             ChangeColor(Color.cyan);
         }
 
+        // 설치 함수
         public virtual void Build()
         {
             WorkingUnitManager.Instance.WorkPrioritySetting(this);
